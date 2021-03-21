029c582fa762a3b61a59f94b937560387ecf9d82
Vương Đức Hiếu <74673530+vuongduchieu0602@users.noreply.github.com>
2021-03-22 00:18:25 +0700

    msg

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+//XMLHttpRequest, fetch, axios,..
 
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars({
@@ -17,13 +24,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
